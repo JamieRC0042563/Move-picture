8f893790554412ef385c8d63002abd18de6dd42b
Jamie Salter <RC0042563@student.riversidecollege.ac.uk>
2020-12-07 15:53:08 +0000

    move picture left

--- Move picture/Form1.cs
+++ Move picture/Form1.cs
@@ -19,7 +19,7 @@ namespace Move_picture
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            pictureBox1.Location = new Point(pictureBox1.Location.X + 5, pictureBox1.Location.Y);
+            pictureBox1.Location = new Point(pictureBox1.Location.X - 60, pictureBox1.Location.Y);
         }
     }
 }
